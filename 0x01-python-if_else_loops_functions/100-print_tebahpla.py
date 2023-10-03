4e92a96fed68e8f196c19890c79dafd4200f3f03
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-10-03 21:29:33 +0300

    updated permissions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-for c in range(90, 64, -1):
-    x = 0
-    if (c%2 == 0):
-        x = 32
-    print({}.format(chr(c+x)), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
