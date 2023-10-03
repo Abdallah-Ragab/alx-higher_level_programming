f13acaf5b21526ff817a1920efb36e842c475b4f
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-10-03 20:55:25 +0300

    fixes

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 for c in range(90, 64, -1):
-    if (c%2 != 0):
-        print(chr(c), end="")
-    else:
-        print(chr(c+32), end="")
+    if (c%2 == 0):
+        c += 32
+    print(chr(c), end="")
