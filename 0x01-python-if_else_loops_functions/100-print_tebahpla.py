c7cf90f8997a814669dfdc1208863fc6042caef9
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-10-03 20:53:05 +0300

    fixes

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# Author -Bamidele Adefolaju
 
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+for c in range(90, 64, -1):
+    if (c%2 != 0):
+        print(chr(c), end="")
+    else:
+        print(chr(c+32), end="")
