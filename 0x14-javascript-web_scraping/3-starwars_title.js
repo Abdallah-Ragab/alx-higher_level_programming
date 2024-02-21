8488d19cccd600640ed757f0ec3923da35c04028
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-02-22 00:33:03 +0200

    0x14-javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
+
+request.get(API_URL + episodeNum).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
